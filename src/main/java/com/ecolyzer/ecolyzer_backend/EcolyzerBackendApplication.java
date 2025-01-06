3aac2705a6a4d48e37e6e3f0a3a0bd61847f768c
Douae sb <douae.sb411@gmail.com>
2025-01-06 13:53:17 +0100

    create project

---
+++ src/main/java/com/ecolyzer/ecolyzer_backend/EcolyzerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ecolyzer.ecolyzer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcolyzerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcolyzerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecolyzer/ecolyzer_backend/EcolyzerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecolyzer.ecolyzer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcolyzerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
